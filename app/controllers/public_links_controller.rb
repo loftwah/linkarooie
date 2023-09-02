911e7f6dfeb4f621a573335bc740030decc6f7a6
Dean Lofts <dean@deanlofts.xyz>
2023-09-03 03:03:29 +1000

    add nicer url names

--- app/controllers/public_links_controller.rb
+++ app/controllers/public_links_controller.rb
@@ -2,7 +2,8 @@ class PublicLinksController < ApplicationController
     skip_before_action :authenticate_user!, only: [:show]  # Skip Devise's authentication for show action
   
     def show
-      @public_links = Link.where(user_id: params[:user_id], public: true)
-    end
+        user = User.find_by!(username: params[:username])
+        @public_links = Link.where(user_id: user.id, public: true)
+      end
   end
   
