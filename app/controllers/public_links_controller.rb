78dfcd22f198f38d31584c6621c5f583bcd2b902
Dean Lofts <dean@deanlofts.xyz>
2023-09-06 20:41:44 +1000

    sweet sharing icon and modal for qr codes

--- app/controllers/public_links_controller.rb
+++ app/controllers/public_links_controller.rb
@@ -1,9 +1,19 @@
+# PublicLinksController
+require 'rqrcode'
+
 class PublicLinksController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:show]  # Skip Devise's authentication for show action
-  
-    def show
-      @user = User.find_by!(username: params[:username])
-      @public_links = Link.where(user_id: @user.id, public: true)
-    end
+  skip_before_action :authenticate_user!, only: [:show]
+
+  def show
+    @user = User.find_by!(username: params[:username])
+    @public_links = Link.where(user_id: @user.id, public: true)
+    @qr = RQRCode::QRCode.new(request.original_url)
+    @svg = @qr.as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 6,
+      standalone: true
+    )
   end
-  
+end
