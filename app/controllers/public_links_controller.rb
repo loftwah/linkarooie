57104519f58c9d372d82f5460a5542f037dce9cf
Dean Lofts <dean@deanlofts.xyz>
2023-09-06 19:25:12 +1000

    enable custom background settings for non-logged in users

--- app/controllers/public_links_controller.rb
+++ app/controllers/public_links_controller.rb
@@ -2,8 +2,8 @@ class PublicLinksController < ApplicationController
     skip_before_action :authenticate_user!, only: [:show]  # Skip Devise's authentication for show action
   
     def show
-        user = User.find_by!(username: params[:username])
-        @public_links = Link.where(user_id: user.id, public: true)
-      end
+      @user = User.find_by!(username: params[:username])
+      @public_links = Link.where(user_id: @user.id, public: true)
+    end
   end
   
