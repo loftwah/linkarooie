775c14cfad35052adbd76c0b0d70a0b60b9e2895
Dean Lofts <dean@deanlofts.xyz>
2023-09-06 18:40:47 +1000

    fix broken hamburger menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
-import "bootstrap"
+import '@popperjs/core';
+import 'bootstrap'
+
+document.addEventListener("turbolinks:load", () => {
+    const navbarToggle = document.getElementById("navbarToggle");
+    const navbarMenu = document.getElementById("navbarNav");
+  
+    if (navbarToggle && navbarMenu) {
+      navbarToggle.addEventListener("click", () => {
+        navbarMenu.classList.toggle("show");
+      });
+    }
+  });  
+  
