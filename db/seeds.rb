13c809fb31147b58f7aae973ffefbd99a578c5de
Dean Lofts <dean@deanlofts.xyz>
2023-09-06 11:12:15 +1000

    add default user and fix broken avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+default_user = User.find_or_create_by(email: 'loftwah@linkarooie.com') do |user|
+    user.password = 'Password01'
+    user.password_confirmation = 'Password01'
+    # add any other user attributes here
+  end
+  
