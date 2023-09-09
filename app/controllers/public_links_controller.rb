c99937696a0eb32bd1e3a12d69a25d85c6977cba
Dean Lofts <dean@deanlofts.xyz>
2023-09-09 15:31:01 +1000

    prevent unknown user error

--- app/controllers/public_links_controller.rb
+++ app/controllers/public_links_controller.rb
@@ -5,15 +5,19 @@ class PublicLinksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
-    @user = User.find_by!(username: params[:username])
-    @public_links = Link.where(user_id: @user.id, public: true)
-    @qr = RQRCode::QRCode.new(request.original_url)
-    @svg = @qr.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 6,
-      standalone: true
-    )
-  end
+    @user = User.find_by(username: params[:username])
+    if @user.nil?
+      redirect_to root_path, alert: "User not found."
+    else
+      @public_links = Link.where(user_id: @user.id, public: true)
+      @qr = RQRCode::QRCode.new(request.original_url)
+      @svg = @qr.as_svg(
+        offset: 0,
+        color: '000',
+        shape_rendering: 'crispEdges',
+        module_size: 6,
+        standalone: true
+      )
+    end
+  end  
 end
