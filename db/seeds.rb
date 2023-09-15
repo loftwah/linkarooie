1db6ffd864477c15c3dd2647a3d0104f35db9ed7
Dean Lofts <dean@deanlofts.xyz>
2023-09-15 23:20:18 +1000

    proompter was wrong lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ default_user = User.find_or_create_by(email: 'loftwah@linkarooie.com') do |user|
   user.username = 'loftwah'
   user.first_name = 'Dean'
   user.last_name = 'Lofts'
-  user.short_description = 'DevOps Engineer, Promoter and a big fan of Open Source.'
+  user.short_description = 'DevOps Engineer, Proompter and a big fan of Open Source.'
   user.tags = 'AWS,DevOps,GitHub,Open Source,Ruby,Ruby on Rails'
 end
 
