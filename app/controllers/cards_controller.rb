4b4e82b585b5667abad6ed66c223557e9ea6efc9
Dean Lofts <dean@deanlofts.xyz>
2023-09-15 21:23:33 +1000

    fix these

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -55,7 +55,7 @@ class CardsController < ApplicationController
     end
   
     def card_params
-      params.require(:card).permit(:name, :description)
-    end
+      params.require(:card).permit(:content, :position, :kanban_column_id)
+    end    
   end
   
