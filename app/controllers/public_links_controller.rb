dec297a01213e70edf599119dce7673327d924ec
Dean Lofts <dean@deanlofts.xyz>
2023-09-16 00:57:45 +1000

    make secret link public

--- app/controllers/public_links_controller.rb
+++ app/controllers/public_links_controller.rb
@@ -2,7 +2,7 @@
 require 'rqrcode'
 
 class PublicLinksController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  skip_before_action :authenticate_user!, only: [:show, :show_secret]
 
   def show
     @user = User.find_by(username: params[:username])
