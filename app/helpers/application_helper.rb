0ad3e1f2ae5627a3cf0a113c750136fa5b3b253e
Dean Lofts <dean@deanlofts.xyz>
2023-09-16 15:44:27 +1000

    fixed for background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
-end
+    def background_style(user)
+      styles = []
+      if user&.background_image&.attached?
+        styles << "background-image: url(#{url_for(user.background_image)});"
+        styles << "background-size: cover;"
+        styles << "background-position: center;"
+        styles << "background-repeat: no-repeat;"
+        styles << "background-attachment: fixed;"
+      end
+      styles << "background-color: #{user.background_color};" if user&.background_color.present?
+      styles.join(' ')
+    end
+  end
