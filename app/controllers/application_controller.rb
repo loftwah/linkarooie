2aa52028a9355e489e2a0172b5ab083366eae842
Dean Lofts <dean@deanlofts.xyz>
2023-10-02 16:16:17 +1100

    refactor and improve user being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :set_global_user
   
-  private
-  
-  def set_global_user
-    @user = current_user if user_signed_in?
-  end
 end
