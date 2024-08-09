23c13c6922284d160bbe3d9dca41078e972c13fe
Dean Lofts <dean@deanlofts.xyz>
2024-08-09 12:56:26 +1000

    accessibility

--- app/helpers/open_graph_helper.rb
+++ app/helpers/open_graph_helper.rb
@@ -5,6 +5,7 @@ module OpenGraphHelper
     content_for :og_title, user.full_name
     content_for :og_description, user.description.truncate(160)
     content_for :og_image, url_for("/uploads/og_images/#{user.username}_og.png")
+    content_for :og_image_alt, "#{user.full_name}'s profile image"
     content_for :og_url, user_links_url(user.username)
   end
 end
