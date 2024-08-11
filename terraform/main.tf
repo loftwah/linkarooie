9451242482686b6602d58c5db98bce16b28649a7
Dean Lofts <dean@deanlofts.xyz>
2024-08-11 18:23:57 +1000

    yolo

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,13 @@ resource "digitalocean_droplet" "web" {
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-22-04-x64"
   ssh_keys = [var.ssh_key_id]
-  user_data = <<-EOF
-              #!/bin/bash
-              # Update package list
-              apt-get update
+  user_data = <<EOF
+#!/bin/bash
+# Update package list
+apt-get update
 
-              # Install Docker using get.docker.com script
-              curl -fsSL https://get.docker.com -o get-docker.sh
-              sh get-docker.sh
-              EOF
+# Install Docker using get.docker.com script
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+EOF
 }
