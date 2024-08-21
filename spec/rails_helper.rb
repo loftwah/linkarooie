843b7c767e4f7f59a8ef5fe550c49841b9d07c38
Dean Lofts <dean@deanlofts.xyz>
2024-08-21 12:25:40 +1000

    try again

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,20 +1,13 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'devise'
 require 'factory_bot_rails'
 
-# Add additional requires below this line. Rails is not loaded until this point!
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories.
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -27,11 +20,15 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
-  # Include Devise test helpers
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
 
-  # Include FactoryBot methods
   config.include FactoryBot::Syntax::Methods
+
+  # Precompile assets before running tests
+  config.before(:suite) do
+    Rails.application.load_tasks
+    Rake::Task['assets:precompile'].invoke
+  end
 end
