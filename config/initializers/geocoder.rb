feecf99246d997fdecad50c497eba10be035526c
Dean Lofts <dean@deanlofts.xyz>
2024-08-21 23:01:12 +1000

    quick fix

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   use_https: true,            # use HTTPS for lookup requests? (if supported)
   http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: nil,               # API key for geocoding service
+  api_key: ENV['GEOCODER_API_KEY'],  # API key for geocoding service
   cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
