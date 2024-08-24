cc74d5ede5628142adf583be204929ba762eb131
Dean Lofts <dean@deanlofts.xyz>
2024-08-24 18:08:35 +1000

    fix tests

--- spec/requests/analytics_spec.rb
+++ spec/requests/analytics_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Analytics', type: :request do
-  describe 'GET /index' do
+  describe 'GET /:username/analytics' do
     let(:user) { create(:user) }
 
     before do
@@ -9,9 +9,28 @@ RSpec.describe 'Analytics', type: :request do
     end
 
     it 'returns http success' do
-      get analytics_path
-      puts response.body # Add this line to print the response body in case of an error
+      get user_analytics_path(username: user.username)
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct content' do
+      get user_analytics_path(username: user.username)
+      expect(response.body).to include('Analytics')  # Adjust this to match your actual content
+    end
+
+    context "when viewing another user's analytics" do
+      let(:other_user) { create(:user, public_analytics: true) }
+
+      it "allows viewing public analytics" do
+        get user_analytics_path(username: other_user.username)
+        expect(response).to have_http_status(:success)
+      end
+
+      it "redirects when trying to view private analytics" do
+        other_user.update(public_analytics: false)
+        get user_analytics_path(username: other_user.username)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
