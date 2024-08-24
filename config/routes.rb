9acfc1f8ede8d1fa68dd8f51f3359d7322f43f8f
Dean Lofts <dean@deanlofts.xyz>
2024-08-25 00:42:24 +1000

    add theme support

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,33 @@
 Rails.application.routes.draw do
   get 'analytics/index'
-  
-  # Use the custom registrations controller in all environments
+
+  # Devise routes for user registration
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
+  # Routes for links with standard RESTful actions
   resources :links do
     member do
       get :track_click
     end
   end
 
+  # Sidekiq monitoring
   require 'sidekiq/web'
   require 'sidekiq-scheduler/web'
   mount Sidekiq::Web => '/sidekiq'
 
-  # Other routes
+  # Custom routes for user-specific views and analytics
   get '/:username/analytics', to: 'analytics#index', as: :user_analytics
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '/:username(/:theme)', to: 'links#user_links', as: :user_links, constraints: { theme: /retro|win95|win98/ }
+
+  # Health check route
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Root route
   root to: 'pages#home'
-  resources :links, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  # Additional resources
   resources :achievements, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  get '/:username', to: 'links#user_links', as: 'user_links'
 end
