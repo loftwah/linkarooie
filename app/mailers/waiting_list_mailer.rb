2e15cb10a0d27418b99309319710a14f7a861ea1
Dean Lofts <dean@deanlofts.xyz>
2024-09-04 22:44:07 +1000

    add waiting list mailer

---
+++ app/mailers/waiting_list_mailer.rb
@@ -0,0 +1,13 @@
+# app/mailers/waiting_list_mailer.rb
+class WaitingListMailer < ApplicationMailer
+  default from: 'loftwah@linkarooie.com'
+
+  def daily_report(email)
+    @waiting_list = WaitingList.all
+    @total_count = @waiting_list.count
+    @today_count = @waiting_list.where('created_at >= ?', Time.zone.now.beginning_of_day).count
+    @url = 'https://linkarooie.com'
+    
+    mail(to: email, subject: 'Daily Waiting List Report - Linkarooie')
+  end
+end
