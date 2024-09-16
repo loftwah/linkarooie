d2924347488a3a0b85b85acb9542b3b4a51e5d98
Dean Lofts <dean@deanlofts.xyz>
2024-09-16 19:48:46 +1000

    open graph image fallback

--- app/services/open_graph_image_generator.rb
+++ app/services/open_graph_image_generator.rb
@@ -11,43 +11,43 @@ class OpenGraphImageGenerator
     template_path = Rails.root.join('app', 'assets', 'images', 'og_template.png')
     output_path = Rails.root.join('public', 'uploads', 'og_images', "#{@user.username}_og.png")
     image = MiniMagick::Image.open(template_path)
-    
-    avatar = download_image(@user.avatar_url)
-
-    # Resize avatar and add a white square border
+  
+    # Skip download if the avatar is the fallback image
+    avatar = if @user.avatar_url == User::FALLBACK_AVATAR_URL
+               MiniMagick::Image.open(Rails.root.join('public', User::FALLBACK_AVATAR_URL))
+             else
+               download_image(@user.avatar_url)
+             end
+  
+    # Resize avatar and continue as before
     avatar.resize "#{AVATAR_SIZE}x#{AVATAR_SIZE}"
     avatar.combine_options do |c|
       c.bordercolor 'white'
       c.border BORDER_SIZE
     end
-
-    # Generate the final image with all elements
+  
     image.combine_options do |c|
       c.font "Liberation-Sans"
-      
-      # Centered avatar
+  
       c.gravity 'Center'
       c.draw "image Over 0,-100 #{AVATAR_SIZE + 2 * BORDER_SIZE},#{AVATAR_SIZE + 2 * BORDER_SIZE} '#{avatar.path}'"
-
-      # Centered Full name
+  
       c.fill '#BEF264'
       c.pointsize '40'
       c.draw "text 0,50 '#{@user.full_name}'"
-
-      # Centered Username with "@" symbol
+  
       c.pointsize '28'
       c.draw "text 0,100 '@#{@user.username}'"
-
-      # Centered Tags, in white
+  
       tag_text = @user.parsed_tags.join(' | ')
       c.fill 'white'
       c.pointsize '20'
       c.draw "text 0,150 '#{tag_text}'"
     end
-
+  
     image.write(output_path)
     output_path
-  end
+  end  
 
   private
 
