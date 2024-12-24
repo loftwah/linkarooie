9cf83bec4e966dde97eed0cc65bcce07c953012a
Dean Lofts <dean@deanlofts.xyz>
2024-12-24 18:26:54 +1100

    tests pass

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,10 +2,10 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.enable_reloading = false
-  config.assets.compile = true
-  config.assets.digest = true
-  config.eager_load = ENV["CI"].present?
-
+  
+  # This is the key change - we need eager loading in test for Devise
+  config.eager_load = true
+  
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
     "cache-control" => "public, max-age=#{1.hour.to_i}"
@@ -21,12 +21,11 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :test
 
+  # Mail config
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   config.active_support.deprecation = :stderr
   config.active_support.report_deprecations = true
-
-  # Uncomment if needed:
-  # config.i18n.raise_on_missing_translations = true
-  # config.action_view.annotate_rendered_view_with_filenames = true
 
   config.action_controller.raise_on_missing_callback_actions = true
 end
